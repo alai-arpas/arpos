295c6e2d4df983d251bcdba1c39caaa04cf4b76e
Andrea Lai <alai.arpas@gmail.com>
2023-08-07 07:35:55 +0200

    spiegazioni in cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,11 @@
-git tag -a "v0.1.1" -m "v0.1.1"
-
-git push origin "v0.1.1"
+# Questi sono i comandi utilizzati in
+# make pub
 
 mix hex.build
+# git tag -a $(versione) -m $(versione)
+git tag -a "v0.1.0" -m "v0.1.0"
+
+# git push origin "v0.1.1"
+git push origin $(versione)
+
 mix hex.publish
